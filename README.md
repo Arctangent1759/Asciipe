45dd6568eb097d0354115fd24dcc65ce49b8fb42
Taylor Money <git@taylorlmoney.com>
2014-03-01 12:18:18 -0800

    Implemented skeleton api

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 AsciiSkype
 ==========
 Words words text text
+
+
+API
+===
+
+### /
+
+initiate call, should return "asciipe 0" if theres an asciipe server, with version 0
+
+### /get/frame/
+
+returns an ascii frame, with \n as linebreaks
+
+### /get/user/
+
+returns the user name
--- client.py
+++ client.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def init():
+    return "asciipe"
+
+@app.route("/get/frame/")
+def get_frame():
+    return "FRAME as ASCII"
+
+@app.route("/get/user/")
+def get_user():
+    try:
+        with open("user") as user_data:
+            return user_data.read()
+    except Error:
+        return "Private Name"
 
 if __name__ == "__main__":
     app.run()
