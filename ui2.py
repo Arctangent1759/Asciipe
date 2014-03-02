ffd29642ea843adf04a40bf351664eb03fcbcfe3
Alex Yang <alex.y9264@gmail.com>
2014-03-01 16:17:19 -0800

    resize handled

--- ui2.py
+++ ui2.py
@@ -35,32 +35,6 @@ class UI(Thread):
 
     def stop(self):
         self.isRunning = False
-"""
-try:
-    s= curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    s.keypad(1)
-    curses.start_color()
-    curses.curs_set(0)
-
-    ui = UI(s)
-    ui.start()
-
-    sleep(15)
-
-except Exception as e:
-    print e
-except KeyboardInterrupt as e:
-    print "Bye!"
-finally:
-    ui.stop()
-
-    s.keypad(0)
-    curses.nocbreak();
-    curses.echo()
-    curses.endwin()
-"""
 
 def main(w):
 
@@ -73,21 +47,14 @@ def main(w):
 
     w.nodelay(1)
     dim = (0, 0)
-    """
     def handle(*args):
         w.erase()
         w.refresh()
-    """
-    def handle(*args):
-        ui.stop()
-        sleep(1000)
-        ui.start()
 
     signal.signal(signal.SIGWINCH, handle)
 
     handle()
     try:
-        ui.start()
         while True:
             w.addstr(0,0,cat)
             w.refresh()
@@ -102,51 +69,5 @@ def main(w):
         curses.echo()
         curses.endwin()
 
-    """
-    while True:
-        w.refresh()
-    """
-    """
-    try:
-        ui = UI(w)
-        ui.start()
-        #sleep(15)
-    except Exception as e:
-        print e
-    except KeyboardInterrupt as e:
-        ui.stop()
-    finally:
-        ui.stop()
-        w.keypad(0)
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
-    """
-"""
-    try:
-        s= curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        s.keypad(1)
-        curses.start_color()
-        curses.curs_set(0)
-
-        ui = UI(s)
-        ui.start()
-
-        sleep(15)
-
-    except Exception as e:
-        print e
-    except KeyboardInterrupt as e:
-        print "Bye!"
-    finally:
-        ui.stop()
-
-        s.keypad(0)
-        curses.nocbreak();
-        curses.echo()
-        curses.endwin()
-"""
 if __name__ == '__main__':
     curses.wrapper(main)
