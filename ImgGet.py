2a86135482e98693d682bd66bd444b5e29967b15
Alexander Chu <alex.p.chu@gmail.com>
2014-03-01 20:26:46 -0800

    binarized

--- ImgGet.py
+++ ImgGet.py
@@ -1,29 +1,6 @@
 import numpy as np
 import cv2
 
-COLORS = [
-    np.array([0, 0, 0]),
-    np.array([170, 0, 0]),
-    np.array([0, 170, 0]),
-    np.array([170, 85, 0]),
-    np.array([0, 0, 170]),
-    np.array([170, 0, 170]),
-    np.array([0, 170, 170]),
-    np.array([170, 170, 170]),
-    np.array([255, 255, 255]),
-    ]
-
-def getClosestColor(x):
-    bestIndex = -1
-    minDist = float("infty")
-    for i in range(len(COLORS)):
-        diff = COLORS[i]-x
-        normSq = np.dot(diff,diff)
-        if normSq < minDist:
-            minDist = normSq
-            bestIndex = i
-    return i
-
 class ImgGetter():
     TRUECHAR="+"
     FALSECHAR="+"
@@ -38,13 +15,13 @@ class ImgGetter():
             raise Exception("Failed to acquire webcam image.")
 
         #Resize and compute to greyscale
-        data = cv2.cvtColor(cv2.resize(raw_data,(3*40,2*40)),cv2.COLOR_BGR2GRAY)
+        grey = cv2.cvtColor(cv2.resize(raw_data,(3*40,2*40)),cv2.COLOR_BGR2GRAY)
 
         #Compute hpfs
-        canny = cv2.Canny(data,100,200)
+        canny = cv2.Canny(grey,100,200)
 
-        width = len(data[0])
-        height = len(data)
+        width = len(grey[0])
+        height = len(grey)
 
         out = ""
 
@@ -56,9 +33,9 @@ class ImgGetter():
                 block_c = np.ndarray.flatten(canny[y:y+blockIncY,x:x+blockIncX])
 
                 if sum(block_c)==0:
-                    out+="\033[0;34m{0}".format(self.FALSECHAR)
+                    out+=str(0)
                 else:
-                    out+="\033[1;32m{0}".format(self.TRUECHAR)
+                    out+=str(1)
             out+="\n"
         return out
 
